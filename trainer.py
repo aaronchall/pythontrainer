8ac3e3f529f161a0fda389995e0464f3c9db2687
AK <andrei.avk@gmail.com>
2015-05-03 17:02:01 -0400

    fixed dict and trailing "s" in commands issue

--- trainer.py
+++ trainer.py
@@ -12,12 +12,12 @@ memory = collections.deque(maxlen=10)
 
 def doc_quiz(quiz_type):
     while True:
-        fn = random.choice(data.data[quiz_type])
+        fn = random.choice(list(data.data[quiz_type]))
         if fn in memory:
             continue
         memory.append(fn)
         try:
-            inp = input('\nTo which function does this documentation belong?\n\n' + 
+            inp = input('\nTo which function does this documentation belong?\n\n' +
               eval(fn).__doc__.replace(fn, 'foo').replace(fn.capitalize(), 'Foo') + '\n\n> ')
         except Exception as e:
             print(e)
@@ -28,16 +28,17 @@ def doc_quiz(quiz_type):
         else:
              print('No, it is ' + fn)
         time.sleep(.5)
-    
+
 def main():
-    try:  
-        while True:        
+    try:
+        while True:
             inp = input('What core Python would you like to focus on today?: \n'
                         'Functions, Exceptions, Keywords, Datatypes, or Modules? \n> ').lower()
             if inp not in ['functions', 'exceptions', 'keywords', 'modules', 'datatypes']:
                 print('I did not understand that')
             else:
-                doc_quiz(inp.rstrip('s'))
+                # doc_quiz(inp.rstrip('s'))
+                doc_quiz(inp)
 
     except (KeyboardInterrupt, EOFError):
         quit(0)
