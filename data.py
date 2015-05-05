14ab2b3d96120237d804adca8ffc3f6963e2c86b
brettlangdon <brett@blangdon.com>
2015-05-05 19:14:58 -0400

    bunch of pep8 updates

--- data.py
+++ data.py
@@ -1,42 +1,44 @@
 from __future__ import print_function
-import sys
+
+import collections  # Container or Sized
+import inspect  # getdoc
 import keyword
-import string
+import numbers  # Number
 import pkgutil
 import pydoc_data.topics
-import collections # Container or Sized
-import numbers # Number
-import inspect # getdoc
+
 
 if not isinstance(__builtins__, dict):
     __builtins__ = vars(__builtins__)
 
 DATA = {}
-DATA['functions'] = dict((k, inspect.getdoc(v)) for k, v in __builtins__.items() 
-                                    if k[0].islower() 
-                                       and k not in ('copyright', 'credits', 'license'))
+DATA['functions'] = dict((k, inspect.getdoc(v)) for k, v in __builtins__.items()
+                         if k[0].islower()
+                         and k not in ('copyright', 'credits', 'license'))
 
-DATA['datatypes'] = dict(('{0}.{1}'.format(k, attr), inspect.getdoc(method))  
-                           for k, v in __builtins__.items()
-                             if isinstance(v, type) and 
-                                issubclass(v, (collections.Sized, numbers.Number))
-                             #if k[0].islower() and (hasattr(v, '__len__')
-                              #                      or hasattr(v, '__bool__'))
-                               for attr, method in vars(v).items()
-                                 if attr[0].islower())
-DATA['keywords'] = dict((k, pydoc_data.topics.topics[k])  for k in set(keyword.kwlist) & set(pydoc_data.topics.topics))
+DATA['datatypes'] = dict(('{0}.{1}'.format(k, attr), inspect.getdoc(method))
+                         for k, v in __builtins__.items()
+                         if isinstance(v, type) and
+                         issubclass(v, (collections.Sized, numbers.Number))
+                         # if k[0].islower() and (hasattr(v, '__len__')
+                         #                      or hasattr(v, '__bool__'))
+                         for attr, method in vars(v).items()
+                         if attr[0].islower())
+DATA['keywords'] = dict((k, pydoc_data.topics.topics[k])
+                        for k in set(keyword.kwlist) & set(pydoc_data.topics.topics))
 
-DATA['exceptions'] = dict((k,v) for k,v in __builtins__.items()
-                          if k[0].isupper() 
-                          and type(v) == type 
-                          and issubclass(v, BaseException)
-                         )
+DATA['exceptions'] = dict((k, v) for k, v in __builtins__.items()
+                          if k[0].isupper()
+                          and type(v) == type
+                          and issubclass(v, BaseException))
 DATA['modules'] = [i[1] for i in pkgutil.iter_modules()
-                            if i[1][0] != '_']
+                   if i[1][0] != '_']
+
 
 def main():
     import pprint
     pprint.pprint(DATA['keywords'])
-    
+
+
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -1,29 +1,28 @@
 import data
-import trainer
 import unittest
 
-class DataTest(unittest.TestCase):
 
+class DataTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data = data.DATA
 
     def test_data(self):
         self.assertIsInstance(self.data, dict)
-    
+
     def test_data_keys(self):
         self.assertIn('keywords', self.data)
         self.assertIn('exceptions', self.data)
         self.assertIn('functions', self.data)
         self.assertIn('modules', self.data)
         self.assertIn('datatypes', self.data)
-        
+
     def test_functions(self):
         funcs = self.data['functions']
         self.assertIn('min', funcs)
         self.assertIn('type', funcs)
         self.assertIn('enumerate', funcs)
-    
+
     def test_keywords(self):
         kw = self.data['keywords']
         self.assertIn('with', kw)
@@ -32,20 +31,19 @@ class DataTest(unittest.TestCase):
     def test_modules(self):
         mods = self.data['modules']
         self.assertIn('os', mods)
-        #self.assertIn('os.path', mods)
+        # self.assertIn('os.path', mods)
         self.assertIn('string', mods)
 
     def test_datatypes(self):
         datatypes = self.data['datatypes']
-        #self.assertIn('str.maketrans', datatypes) # not in Python 2
+        # self.assertIn('str.maketrans', datatypes) # not in Python 2
         self.assertIn('str.lower', datatypes)
 
     def test_exceptions(self):
         exceptions = self.data['exceptions']
         self.assertIn('BaseException', exceptions)
         self.assertIn('OSError', exceptions)
-        
-        
+
 
 def main():
     unittest.main()
