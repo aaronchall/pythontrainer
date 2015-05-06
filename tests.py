f59a0069d0af272f26cd5177434705b2a88850dc
brettlangdon <brett@blangdon.com>
2015-05-05 20:30:40 -0400

    add BackportTestCase for python 2.6 test support

--- tests.py
+++ tests.py
@@ -1,25 +1,33 @@
 import data
 import unittest
+import sys
 
-_MAX_LENGTH = 80
 
-
-def safe_repr(obj, short=False):
+class BackportTestCase(unittest.TestCase):
     """
-    Backport of unittest.util.safe_repr from python 2.7 for 2.6
-    https://github.com/python/cpython/blob/cc14aa98797433709230c5522261b0e42d9fb284/Lib/unittest/util.py
+    Helper class to provide backport support for `assertIn` and `assertIsInstance`
+    for python 2.6
     """
-    try:
-        result = repr(obj)
-    except Exception:
-        result = object.__repr__(obj)
-        if not short or len(result) < _MAX_LENGTH:
-            return result
-        return result[:_MAX_LENGTH] + ' [truncated]...'
+    MAX_LENGTH = 80
 
+    def __init__(self, *args, **kwargs):
+        super(BackportTestCase, self).__init__(*args, **kwargs)
+        if sys.version_info < (2, 7):
+            self.assertIn = self.assert_in
+            self.assertIsInstance = self.assert_is_instance
 
-class DataTest(unittest.TestCase):
-    data = data.DATA
+    def safe_repr(self, obj, short=False):
+        """
+        Backport of unittest.util.safe_repr from python 2.7 for 2.6
+        https://github.com/python/cpython/blob/cc14aa98797433709230c5522261b0e42d9fb284/Lib/unittest/util.py
+        """
+        try:
+            result = repr(obj)
+        except Exception:
+            result = object.__repr__(obj)
+            if not short or len(result) < self.MAX_LENGTH:
+                return result
+            return result[:self.MAX_LENGTH] + ' [truncated]...'
 
     def assert_in(self, member, container, msg=None):
         """
@@ -29,8 +37,8 @@ class DataTest(unittest.TestCase):
         https://github.com/python/cpython/blob/cc14aa98797433709230c5522261b0e42d9fb284/Lib/unittest/case.py
         """
         if member not in container:
-            standard_msg = '%s not found in %s' % (safe_repr(member),
-                                                   safe_repr(container))
+            standard_msg = '%s not found in %s' % (self.safe_repr(member),
+                                                   self.safe_repr(container))
             self.fail(self._formatMessage(msg, standard_msg))
 
     def assert_is_instance(self, obj, cls, msg=None):
@@ -42,45 +50,49 @@ class DataTest(unittest.TestCase):
         https://github.com/python/cpython/blob/cc14aa98797433709230c5522261b0e42d9fb284/Lib/unittest/case.py
         """
         if not isinstance(obj, cls):
-            standard_msg = '%s is not an instance of %r' % (safe_repr(obj), cls)
+            standard_msg = '%s is not an instance of %r' % (self.safe_repr(obj), cls)
             self.fail(self._formatMessage(msg, standard_msg))
 
+
+class DataTest(BackportTestCase):
+    data = data.DATA
+
     def test_data(self):
-        self.assert_is_instance(self.data, dict)
+        self.assertIsInstance(self.data, dict)
 
     def test_data_keys(self):
-        self.assert_in('keywords', self.data)
-        self.assert_in('exceptions', self.data)
-        self.assert_in('functions', self.data)
-        self.assert_in('modules', self.data)
-        self.assert_in('datatypes', self.data)
+        self.assertIn('keywords', self.data)
+        self.assertIn('exceptions', self.data)
+        self.assertIn('functions', self.data)
+        self.assertIn('modules', self.data)
+        self.assertIn('datatypes', self.data)
 
     def test_functions(self):
         funcs = self.data['functions']
-        self.assert_in('min', funcs)
-        self.assert_in('type', funcs)
-        self.assert_in('enumerate', funcs)
+        self.assertIn('min', funcs)
+        self.assertIn('type', funcs)
+        self.assertIn('enumerate', funcs)
 
     def test_keywords(self):
         kw = self.data['keywords']
-        self.assert_in('with', kw)
-        self.assert_in('if', kw)
+        self.assertIn('with', kw)
+        self.assertIn('if', kw)
 
     def test_modules(self):
         mods = self.data['modules']
-        self.assert_in('os', mods)
+        self.assertIn('os', mods)
         # self.assertIn('os.path', mods)
-        self.assert_in('string', mods)
+        self.assertIn('string', mods)
 
     def test_datatypes(self):
         datatypes = self.data['datatypes']
         # self.assertIn('str.maketrans', datatypes) # not in Python 2
-        self.assert_in('str.lower', datatypes)
+        self.assertIn('str.lower', datatypes)
 
     def test_exceptions(self):
         exceptions = self.data['exceptions']
-        self.assert_in('BaseException', exceptions)
-        self.assert_in('OSError', exceptions)
+        self.assertIn('BaseException', exceptions)
+        self.assertIn('OSError', exceptions)
 
 
 def main():
